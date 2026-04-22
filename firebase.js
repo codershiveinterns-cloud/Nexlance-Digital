c0affdfe87239284a535fc68ca767407114fa02e
codershiveinterns-1285 <codershiveinterns@gmail.com>
2026-04-22 16:00:18 +0530

    include

--- firebase.js
+++ firebase.js
@@ -1,16 +1,9 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth, browserLocalPersistence, setPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics, isSupported as isAnalyticsSupported } from "firebase/analytics";
 
 // Shared Firebase configuration for the frontend auth flow.
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBmOse6NV9Gianc8IkOH_4UpjXYADj-xb4",
   authDomain: "nexlance-digital.firebaseapp.com",
@@ -21,22 +14,17 @@ const firebaseConfig = {
   measurementId: "G-C7H3GKMPWZ"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCv56CN--eQLTCxomNItL2FgLRoIbdsdoM",
-//   authDomain: "nexlance-df59e.firebaseapp.com",
-//   projectId: "nexlance-df59e",
-//   storageBucket: "nexlance-df59e.firebasestorage.app",
-//   messagingSenderId: "480679982312",
-//   appId: "1:480679982312:web:2eb0d840f03c81db49055d",
-//   measurementId: "G-0XG3807L8Q",
-// };
-
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+isAnalyticsSupported()
+  .then((supported) => {
+    if (supported) {
+      getAnalytics(app);
+    }
+  })
+  .catch(() => {});
 
 // Keep users signed in across refreshes in the browser.
 const authReady = setPersistence(auth, browserLocalPersistence).catch((error) => {
